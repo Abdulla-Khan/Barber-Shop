3399160423c7706a0b552d0104ee014fb7f98689
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-09-02 03:53:26 +0500

    Home Page Added

---
+++ public/home/app.js
@@ -0,0 +1,26 @@
+function myFunction(y) {
+    y.classList.toggle("fa-plus");
+
+
+
+    var x = document.getElementById("welcomeDiv");
+
+
+    if (x.style.display == "none") {
+        x.style.display = 'block'
+
+
+
+    } else {
+        x.style.display = "none";
+
+
+
+    }
+}
+
+const menuBtn = document.querySelector('.menu-btn')
+const navlinks = document.querySelector('.nav-links')
+menuBtn.addEventListener('click', () => {
+    navlinks.classList.toggle('mobile-menu')
+})
